4cc578a4193fb2d3c0e2b69c06c507b5aeffec31
Sharens <64214693+Sharens@users.noreply.github.com>
2020-11-07 07:36:40 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int dlugosc, wysokosc, pole_trojkata;
+    dlugosc = 2;
+    wysokosc = 3;
+
+    pole_trojkata = (dlugosc * wysokosc)/2;
+    printf("pole trójkąta wynosi %d",pole_trojkata);
+
+    return 0;
+
+}
